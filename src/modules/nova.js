c532a068195dfb6a07b13e8f0f9b356c3635c8f4
Keanu <keanutimmermans8@gmail.com>
2021-03-04 19:00:05 +0100

    Added usrbg.

--- src/modules/nova.js
+++ src/modules/nova.js
@@ -2,5 +2,6 @@ export default [
   ['KeanuGooseMod/Modules', 'master', '/ConsoleLog'],
   ['KeanuGooseMod/Modules', 'master', '/comfy-theme'],
   ['KeanuGooseMod/Modules', 'master', '/settings-experiment'],
-  ['KeanuGooseMod/Modules', 'master', '/FreeNitro']
+  ['KeanuGooseMod/Modules', 'master', '/FreeNitro'],
+  ['KeanuGooseMod/Modules', 'master', '/usrbg'],
 ];
