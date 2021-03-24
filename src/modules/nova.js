1ff44406745c6366b7c2658e7d17db5c439823c9
フズキ <hades785@gmail.com>
2021-03-25 00:24:30 +0100

    Added commands experiments

--- src/modules/nova.js
+++ src/modules/nova.js
@@ -1,6 +1,7 @@
 export default [
   ['NovaGM/Modules', 'master', '/ConsoleLog'],
   ['NovaGM/Modules', 'master', '/comfy-theme'],
+  ['NovaGM/Modules', 'master', '/commands-experiment'],
   ['NovaGM/Modules', 'master', '/settings-experiment'],
   ['NovaGM/Modules', 'master', '/FreeNitro'],
   ['NovaGM/Modules', 'master', '/usrbg'],
