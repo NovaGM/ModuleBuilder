f39dab4f4039be1f3f4771f323e371c461f34729
Lexi Sother <lexisoth2005@gmail.com>
2021-04-05 19:00:18 +0000

    Added clap PC module

--- src/modules/ports/plugins/pcPlugins.js
+++ src/modules/ports/plugins/pcPlugins.js
@@ -1,5 +1,8 @@
 export default [
   ['RazerMoon/muteNewGuild', 'a8ea528e67089ea00a457623cebbef6a670127fc', '/manifest.json', 'pcPlugin', {
-    authors: ['162970149857656832']
-  }]
+      authors: ['162970149857656832'],
+  }],
+  ['keanuplayz/PC-Clap', 'master', '/manifest.json', 'pcPlugin', {
+      authors: ['717352467280691331'],
+  }],
 ];
