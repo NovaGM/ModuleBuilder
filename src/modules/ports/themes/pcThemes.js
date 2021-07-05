d209c0665ec8bc447b4b8717d5d37299f5d0301c
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-05 20:56:16 +0200

    [Add Theme (PCTheme)] lexisother/dotfiles-discord-theme

--- src/modules/ports/themes/pcThemes.js
+++ src/modules/ports/themes/pcThemes.js
@@ -6,5 +6,8 @@ export default [
 
   ['keanuplayz/usrbg', '', '/powercord_manifest.json', 'pcTheme', {
     authors: ['717352467280691331']
+  }],
+  ['lexisother/dotfiles-discord-theme', '', '/powercord_manifest.json', 'pcTheme', {
+    authors: ['465702500146610176', '364720702252908544']
   }]
 ];
