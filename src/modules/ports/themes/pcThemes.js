f761cfbd56bdbaa6103b04af00986c1a3b2c8cfb
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-06 15:14:32 +0200

    [Update Theme (PCTheme)] lexisother/dotfiles-discord-theme

--- src/modules/ports/themes/pcThemes.js
+++ src/modules/ports/themes/pcThemes.js
@@ -20,6 +20,10 @@ export default [
     'pcTheme',
     {
       authors: ['465702500146610176', '364720702252908544'],
+      images: [
+        'https://media.discordapp.net/attachments/843471565986594816/861969950406213632/dotfiles_preview_one.png',
+        'https://media.discordapp.net/attachments/843471565986594816/861969945713573908/dotfiles_preview_two.png',
+      ],
     },
   ],
 ];
